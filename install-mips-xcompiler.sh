630b467770d6106711ae77a1f0eb861dc92e5fec
Joey Dumont <joey.dumont@gmail.com>
2019-01-26 18:11:09 -0500

    Redirected makepkg logs.

--- install-mips-xcompiler.sh
+++ install-mips-xcompiler.sh
@@ -12,24 +12,24 @@ set -e
 # -- Install mips64-elf-binutils
 git clone https://aur.archlinux.org/mips64-elf-binutils.git
 cd mips64-elf-binutils
-patch -i ../PKGBUILD.patch
-makepkg --noconfirm -sic
+patch -i ../PKGBUILD.patch 
+makepkg --noconfirm -sic > makepkg.log 2>&1
 cd ../
 
 # -- Install mips64-elf-gcc-stage1
 git clone https://aur.archlinux.org/mips64-elf-gcc-stage1.git
 cd mips64-elf-gcc-stage1
-makepkg --noconfirm  -sic
+makepkg --noconfirm  -sic > makepkg.log 2>&1
 cd ../
 
 # -- Install mips64-elf-newlib
 git clone https://aur.archlinux.org/mips64-elf-newlib.git
 cd mips64-elf-newlib
-makepkg --noconfirm -sic
+makepkg --noconfirm -sic > makepkg.log 2>&1
 cd ../
 
 # -- Finally install mips64-elf-gcc
 git clone https://aur.archlinux.org/mips64-elf-gcc.git
 cd mips64-elf-gcc
-makepkg --noconfirm -sic
+makepkg --noconfirm -sic > makepkg.log 2>&1
 cd ../
