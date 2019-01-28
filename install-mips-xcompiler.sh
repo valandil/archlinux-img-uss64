f3945f87e8e6f600983699a740c34bef1be7da06
Joey Dumont <joey.dumont@gmail.com>
2019-01-27 22:33:13 -0500

    Perl test.

--- install-mips-xcompiler.sh
+++ install-mips-xcompiler.sh
@@ -9,10 +9,22 @@
 # -- Fails the script when a command fails.
 set -e 
 
+# -- Bucket that holds the package sources.
+BUCKET_URL='https:\/\/s3.amazonaws.com\/archlinux-n64-packages\/'
+
+
+# -- Function to replace the source of each PKGBUILD.
+# --    $1: Name of file in S3 bucket.
+# --    $2: Path of file.
+function ReplaceSourceWithS3()
+{
+    perl -i.bak -pe 's/(?<=source=\()[^\s\)]+/'"${BUCKET_URL}$1"'/; print $1;' $2
+}
+
 # -- Install mips64-elf-binutils
 git clone https://aur.archlinux.org/mips64-elf-binutils.git
 cd mips64-elf-binutils
-patch -i ../PKGBUILD.patch 
+ReplaceSourceWithS3 'binutils-\${pkgver}.tar.xz' PKGBUILD
 makepkg --noconfirm -sic > makepkg.log 2>&1
 cd ../
 
@@ -25,6 +37,7 @@ cd ../
 # -- Install mips64-elf-newlib
 git clone https://aur.archlinux.org/mips64-elf-newlib.git
 cd mips64-elf-newlib
+ReplaceSourceWithS3 'newlib-\${pkgver}.tar.gz' PKGBUILD
 makepkg --noconfirm -sic > makepkg.log 2>&1
 cd ../
 
