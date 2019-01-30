f34eb13e1eb1d570a2aff569210685a0f86044cb
Joey Dumont <joey.dumont@gmail.com>
2019-01-29 21:18:34 -0500

    WHY IS THIS SO HARD.

--- install-mips-xcompiler.sh
+++ install-mips-xcompiler.sh
@@ -42,7 +42,7 @@ makepkg --noconfirm -sic > makepkg.log 2>&1
 cd ../
 
 # -- Finally install mips64-elf-gcc
-pacman -R --nodeps --nodeps --noconfirm mips64-elf-gcc
+sudo pacman -R --nodeps --nodeps --noconfirm mips64-elf-gcc
 git clone https://aur.archlinux.org/mips64-elf-gcc.git
 cd mips64-elf-gcc
 makepkg --noconfirm -sic
